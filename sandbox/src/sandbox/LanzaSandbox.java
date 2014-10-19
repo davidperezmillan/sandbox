53e1a9bdfec7a149f00913cc12f04314df0f5688
david <david@Asus-F5R>
2014-10-19 19:28:11 +0200

    Lanza

---
+++ sandbox/src/sandbox/LanzaSandbox.java
@@ -0,0 +1,10 @@
+package sandbox;
+
+public class LanzaSandbox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo sandbox");
+	}
+
+}
